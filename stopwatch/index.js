b3d9eb139c683bf59847f5821504a94080b65bdb
samvaidya961 <67541221+samvaidya961@users.noreply.github.com>
2021-09-28 17:25:10 +0530

    Add files via upload

---
+++ stopwatch/index.js
@@ -0,0 +1,50 @@
+let min = 0;
+let sec = 0;
+
+let minstext = document.getElementById('mins');
+let secstext = document.getElementById('secs');
+
+const startbtn = document.getElementById('start');
+const stopbtn = document.getElementById('stop');
+const resetbtn = document.getElementById('reset');
+
+var Interval;
+
+startbtn.addEventListener("click", function () {
+    clearInterval(Interval);
+    Interval = setInterval(start, 1000);
+})
+
+stopbtn.addEventListener("click", function () {
+    clearInterval(Interval);
+})
+
+resetbtn.addEventListener("click", function () {
+    clearInterval(Interval);
+    min = "00";
+    sec = "00";
+    minstext.textContent = min;
+    secstext.textContent = sec;
+})
+
+function start() {
+
+    sec++;
+
+    if (sec < 10) {
+        secstext.textContent = "0" + sec;
+    }
+    if (sec >= 10) {
+        secstext.textContent = sec;
+    }
+    if (sec > 59) {
+        min++;
+        minstext.textContent = "0" + min;
+        sec = 0;
+        secstext.textContent = "0" + 0;
+    }
+    if (min > 10) {
+        minstext.textContent = min;
+    }
+}
+
